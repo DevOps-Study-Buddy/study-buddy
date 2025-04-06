cd77f5c9b349cc3e91d9c11de68d73a1c9bd6fd9
Seyi Olawepo <sheyi103@outlook.com>
2025-04-06 21:55:13 +0100

    build: generated the jar file

--- study-buddy-backend/src/main/java/com/buddy/studybuddy/dtos/ErrorDTO.java
+++ study-buddy-backend/src/main/java/com/buddy/studybuddy/dtos/ErrorDTO.java
@@ -1,13 +1,26 @@
 package com.buddy.studybuddy.dtos;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ErrorDTO {
     private String code;
     private String message;
+
+    public ErrorDTO() {
+
+    }
+
+    public Object setCode(String code) {
+        this.code = code;
+        return null;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public String getCode() {
+        return code;
+    }
+    public String getMessage() {
+        return message;
+    }
 }
