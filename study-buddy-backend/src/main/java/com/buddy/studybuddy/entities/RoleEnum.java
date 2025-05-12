47789f0f83d26fa39b08b7b2934f2cac4adb124b
Seyi Olawepo <sheyi103@outlook.com>
2025-05-12 23:12:06 +0100

    evil

--- study-buddy-backend/src/main/java/com/buddy/studybuddy/entities/RoleEnum.java
+++
@@ -1,7 +0,0 @@
-package com.buddy.studybuddy.entities;
-
-public enum RoleEnum {
-    USER,
-    ADMIN,
-    SUPER_ADMIN
-}
--- study-buddy-backend/studybuddy/src/main/java/com/buddy/Main.java
+++
@@ -1,7 +0,0 @@
-package com.buddy;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
